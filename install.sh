d11f56a952609bd3718aa70d92dce82f6f51846f
ThiagoLira <thlira15@gmail.com>
2017-11-03 18:33:09 -0200

    now autocompleteme compiles itself

--- install.sh
+++ install.sh
@@ -36,3 +36,6 @@ source ~/.dotfiles/bin/link.sh
 # now we run nvim and vim to install the plugins
 nvim +PlugInstall +qall
 vim +PlugInstall +qall
+# compile youcompleteme
+cd ~/.dotfiles/config/nvim/plugged/YouCompleteMe
+./install.py
