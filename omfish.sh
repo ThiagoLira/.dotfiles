625fba9ff15c2c46bcd7d8dabeff44628aa69f57
ThiagoLira <thlira15@gmail.com>
2017-11-08 01:19:31 +0000

    added fonts to omf

--- omfish.sh
+++ omfish.sh
@@ -1,2 +1,6 @@
 curl -L https://get.oh-my.fish > install
 fish install --path=~/.local/share/omf --config=~/.dotfiles/config/omf
+omf install z
+omf install fonts
+fonts install --powerline Inconsolata
+omf install agnoster
