492d8ad8103178a01122b74294e1e48a8cc182ef
ThiagoLira <thlira15@gmail.com>
2018-04-21 22:22:47 +0000

    fixed comment on fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,30 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG '/home/thiago/.dotfiles/config/omf'
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+# Readline colors
+set -g fish_color_autosuggestion 555 yellow
+set -g fish_color_command 5f87d7
+set -g fish_color_comment 808080
+set -g fish_color_cwd 87af5f
+set -g fish_color_cwd_root 5f0000
+set -g fish_color_error 870000 --bold
+set -g fish_color_escape af5f5f
+set -g fish_color_history_current 87afd7
+set -g fish_color_host 5f87af
+set -g fish_color_match d7d7d7 --background=303030
+set -g fish_color_normal normal
+set -g fish_color_operator d7d7d7
+set -g fish_color_param 5f87af
+set -g fish_color_quote d7af5f
+set -g fish_color_redirection normal
+set -g fish_color_search_match --background=purple
+set -g fish_color_status 5f0000
+set -g fish_color_user 5f875f
+set -g fish_color_valid_path --underline
