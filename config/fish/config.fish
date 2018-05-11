ecbf9845d904b5c4e5a6807ce62d2cd62d8aa672
ThiagoLira <thlira15@gmail.com>
2018-05-11 01:28:12 +0000

    for now, lets not use omf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,15 @@
 # Path to Oh My Fish install.
 set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
+and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+or set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
 set -gx OMF_CONFIG '/home/thiago/.dotfiles/config/omf'
 
 # Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
+# source $OMF_PATH/init.fish
+
+
 # Readline colors
 set -g fish_color_autosuggestion 555 yellow
 set -g fish_color_command 5f87d7
@@ -28,3 +30,6 @@ set -g fish_color_search_match --background=purple
 set -g fish_color_status 5f0000
 set -g fish_color_user 5f875f
 set -g fish_color_valid_path --underline
+ 
+fish_vi_key_bindings
+
--- config/omf/init.fish
+++ config/omf/init.fish
@@ -4,5 +4,5 @@ function mkd
 	mkdir -p $argv; and cd $argv
 end
 
-set -x LC_ALL en_US.UTF-8
-set -x LC_CTYPE en_US.UTF-8
+# set -x LC_ALL en_US.UTF-8
+# set -x LC_CTYPE en_US.UTF-8
