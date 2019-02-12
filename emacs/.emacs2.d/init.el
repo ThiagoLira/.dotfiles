8a2009ff21345977a99d5651f12d6b59517314f7
ThiagoLira <thlira15@gmail.com>
2019-02-12 11:52:52 -0200

    treemacs

--- emacs/.emacs2.d/init.el
+++ emacs/.emacs2.d/init.el
@@ -12,7 +12,7 @@
 
 
 (setq init-message
-"                                                                
+"
  |  _ \                                                                   
  | |_) |  ___   _ __  __ _                                                
  |  _ <  / _ \ | '__|/ _` |                                               
@@ -410,10 +410,13 @@ Repeated invocations toggle between the two most recently open buffers."
     "bR"  'rename-file-and-buffer
     "br"  'revert-buffer
 
+    "qq" 'kill-emacs
+    
  ;; bind to double key press
   "ff"  'counsel-find-file  ; find file using ivy
   "fr"	'counsel-recentf    ; find recently edited files
   "pf"  'counsel-git        ; find file in git project
+  "ft"  'treemacs ;;treemacs toogle
  ;; Applications
  "a" '(:ignore t :which-key "Applications")
  "ar" 'ranger
@@ -463,6 +466,69 @@ _k_: delete up     ^ ^                ^ ^
   "zoom"
   ("g" text-scale-increase "in")
   ("l" text-scale-decrease "out"))
+
+
+
+;; TREEMACS
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if (executable-find "python") 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-display-in-side-window        t
+          treemacs-file-event-delay              5000
+          treemacs-file-follow-delay             0.2
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-desc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))))
+
+
+
+(use-package treemacs-evil
+  :ensure t
+  :after treemacs evil)
 
 
 
