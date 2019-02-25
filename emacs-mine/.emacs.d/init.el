8e0a01c396f441085f799405e76f0acdc6b4e9c3
ThiagoLira <thlira15@gmail.com>
2019-02-25 12:18:42 -0300

    movement evil windows

--- emacs-mine/.emacs.d/init.el
+++ emacs-mine/.emacs.d/init.el
@@ -284,6 +284,12 @@
         (kbd "j") 'archive-next-line
         (kbd "k") 'archive-previous-line))
 
+   ;; Window Movement 
+     (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+     (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+     (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+     (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
     (evil-set-initial-state 'wdired-mode 'normal)
 
     (evil-set-initial-state 'ert-simple-view-mode 'motion)
