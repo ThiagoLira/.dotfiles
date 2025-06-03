7e0c3b5550268ece2a21e42600972e8f960d7036
Thiago Lira <thlira15@gmail.com>
2025-06-03 20:10:09 -0300

    removed mini.ai

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -703,19 +703,6 @@ require("lazy").setup({
 	{ -- Collection of various small independent plugins/modules
 		"echasnovski/mini.nvim",
 		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
 
 			-- Simple and easy statusline.
@@ -732,9 +719,6 @@ require("lazy").setup({
 			statusline.section_location = function()
 				return "%2l:%-2v"
 			end
-
-			-- more text objects using treesiter
-			require("mini.ai").setup()
 		end,
 	},
 	{ -- Highlight, edit, and navigate code
